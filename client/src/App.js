96a11d18f47a28a159653cfa005994008aa847f4
kiyani <bamba.kiyani@gmail.com>
2019-09-17 23:20:28 -0400

    Create link in MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom'
 
-import MovieList from './Movies/MovieList'
-import Movie from './Movies/Movie'
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -15,12 +15,11 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
       <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" render={(props) => 
-      <Movie {...props} addToSavedList={this.addToSavedList} />} />
+      <Route path="/movie/:id" component={Movie} />
     </div>
   );
 };
 
 export default App;
+
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import axios from 'axios';
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -24,28 +26,7 @@ const MovieList = props => {
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <Link to={`/movies/${movie.id}`}>Click to see Movie</Link>
   );
 }
 
